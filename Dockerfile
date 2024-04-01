05c9ee9172222679b2ed5a2b73dc298408ccfd95
tom maher <tmaher@pw0n.ie>
2024-04-01 04:39:50 +0100

    wip

--- Dockerfile
+++ Dockerfile
@@ -7,10 +7,13 @@ RUN mkdir /app
 COPY "pyproject.toml" "poetry.*" "*.rb" "*.py" "restock_shelf.sh" /app/
 COPY "src/" /app/src/
 
-ENV AUDIBLE_CONFIG_DIR=${AUDIBLE_CONFIG_DIR:-/config} 
+ENV AUDIBLE_CONFIG_DIR=${AUDIBLE_CONFIG_DIR:-/config}
+ENV AUDIBLE_PLUGIN_DIR=${AUDIBLE_PLUGIN_DIR:-/app/src/audible-cli/plugins}
 RUN mkdir -p ${AUDIBLE_CONFIG_DIR}
 WORKDIR /app
 
 RUN poetry install
+
+ENV PATH=/app/.venv/bin:${PATH}
 
 # CMD ["/app/restock_shelf.sh"]
--- restock_shelf.sh
+++ restock_shelf.sh
@@ -2,9 +2,15 @@
 
 target_dir=${target_dir:-/shelf}
 export target_dir
+mkdir -p ${target_dir}/dl ${target_dir}/pt
+cd ${target_dir}/dl
 
-cd /app
-pipenv run ./getlib.py
+audible download --aaxc --pdf --cover --cover-size 500 --chapter \
+    --annotation -j 4 --ignore-podcasts --all
+#    --start-date 2024-01-01 --end-date 2024-03-01
 
-cd "${target_dir}"
-/app/generate_personal_podcast.rb
+audible decrypt --all --dir ${target_dir}/pt \
+    --rebuild-chapters --force-rebuild-chapters
+
+# cd "${target_dir}"
+# /app/generate_personal_podcast.rb
