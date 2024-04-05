2a6177d1502d59d2decd26162d7c7a3297206209
tom maher <tmaher@pw0n.ie>
2024-04-05 17:39:31 +0100

    restock_shelf - fetch library json

--- restock_shelf.sh
+++ restock_shelf.sh
@@ -5,14 +5,17 @@ export target_dir
 mkdir -p "${target_dir}/dl" "${target_dir}/pt"
 cd "${target_dir}/dl" || exit 1
 
-#audible download --aaxc --pdf --cover --cover-size 500 --chapter \
-#    --annotation -j 4 --quality best \
-#    --resolve-podcasts \
-#    --all
-# --start-date 2024-01-01 --end-date 2099-12-31
+audible library export --resolve-podcasts --format json
+
+audible download --aaxc --pdf --cover --cover-size 500 --chapter \
+    --annotation -j 4 --quality best \
+    --filename-mode asin_ascii \
+    --resolve-podcasts \
+    --all \
+    --start-date 2024-01-01 --end-date 2099-12-31
 
 audible decrypt --all --dir "${target_dir}/pt" \
     --rebuild-chapters --force-rebuild-chapters
 
-# cd "${target_dir}"
-# /app/generate_personal_podcast.rb
+cd "${target_dir}/pt" || exit 1
+/app/generate_personal_podcast.rb
