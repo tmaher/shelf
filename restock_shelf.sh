f4151b3efde5645364f7e0127129162c0265717c
tom maher <tmaher@pw0n.ie>
2024-04-09 18:27:05 +0100

    restock_shelf.sh - always rescale image

--- restock_shelf.sh
+++ restock_shelf.sh
@@ -7,6 +7,7 @@
 : "${SHELF_URL_PREFIX:=https://invalid.invalid/}" ; export SHELF_URL_PREFIX
 : "${SHELF_START_DATE:=1901-12-12}" ; export SHELF_START_DATE
 : "${SHELF_END_DATE:=3000-01-01}" ; export SHELF_END_DATE
+: "${SHELF_IMG_DL_SIZE:=1215}" ; export SHELF_IMG_DL_SIZE
 
 cd "${SHELF_TARGET_DIR}" || exit 1
 mkdir -p "assets" "dl"
@@ -19,7 +20,7 @@ audible download \
     --aaxc \
     --pdf \
     --cover \
-    --cover-size 900 \
+    --cover-size "${SHELF_IMG_DL_SIZE}" \
     --chapter \
     --annotation \
     --jobs 4 \
@@ -40,16 +41,17 @@ audible decrypt \
 for book in *.aaxc; do
     img_target="${SHELF_TARGET_DIR}/assets/$(basename "${book}" .aaxc).jpg"
     if [ ! -r "${img_target}" ]; then
-        img_src="$(echo "${book}" | sed -E 's/-AAX_[0-9_]+.aaxc/_(900)/').jpg"
-        if [ -r "${img_src}" ]; then
-            cp "${img_src}" "${img_target}"
-        else
-            ffmpeg -i "${book}" -vcodec copy -frames:v 1 tmp_frame_%d.jpg
-            mv tmp_frame_1.jpg "${img_target}"
-        fi
+        img_regex="s/-AAX_[0-9_]+.aaxc/_(${SHELF_IMG_DL_SIZE})/"
+        img_src="$(echo "${book}" | sed -E "${img_regex}").jpg"
+        : "${img_src:="${book}"}"
+
+        ffmpeg \
+            -i "${book}" \
+            -vf "scale=3000:3000:force_original_aspect_ratio=increase,pad=3000:3000:(ow-iw)/2:(oh-ih)/2" \
+            -frames:v 1 \
+            "${img_target}"
     fi
 done
-rm -f tmp_frame_*.jpg
 
 cd "${SHELF_TARGET_DIR}/assets" || exit 1
 
