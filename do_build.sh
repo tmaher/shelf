8a31ba7f63408ca93c40224bae171cf6b3f08396
tom maher <tmaher@pw0n.ie>
2024-04-20 18:56:51 +0100

    ready to test...

--- do_build.sh
+++ do_build.sh
@@ -12,7 +12,10 @@ APPDIR="$(cd "$(dirname "$0")" && echo "$PWD")" ; export APPDIR
 cd "${APPDIR}" || _error "Unable to cd to '${APPDIR}'"
 
 docker image build -t shelf:dev . && docker run -it \
+    --user 911:911 \
     -v ./tmp:/shelf \
     -v ./config:/config \
+    -v ./.pytest_cache:/app/.pytest_cache \
     --env-file ./secrets.env \
+    -w /app \
     shelf:dev "$@"
