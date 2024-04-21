bff84e56bb5c4d1dc6f6a56781411399ad05e65d
tom maher <tmaher@pw0n.ie>
2024-04-21 17:17:57 +0100

    wip

--- tests/test_shelf.py
+++ tests/test_shelf.py
@@ -5,7 +5,15 @@ from shelf import ShelfFeedGenerator
 class TestSFG:
     @pytest.fixture
     def my_sfg(self):
-        return ShelfFeedGenerator()
+        sfg = ShelfFeedGenerator()
+        sfg.title('bob the angry podcast')
+        return sfg
 
     def test_create(self, my_sfg):
         assert isinstance(my_sfg, ShelfFeedGenerator)
+
+    def test_title(self, my_sfg):
+        assert my_sfg.title() == 'bob the angry podcast'
+
+    def test_ext(self, my_sfg):
+        True
