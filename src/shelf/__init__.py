a544676608c0caefcf6e28850bb20960044896bf
tom maher <tmaher@pw0n.ie>
2024-04-21 19:42:16 +0100

    path stuff

--- src/shelf/__init__.py
+++ src/shelf/__init__.py
@@ -6,3 +6,5 @@ from .shelf_feed_generator import ShelfError, ShelfFeedGenerator
 print("I'm in danger!!!")
 
 __all__ = ["__version__", ShelfFeedGenerator, ShelfError]
+
+__path__ = __import__('pkgutil').extend_path(__path__, __name__)
