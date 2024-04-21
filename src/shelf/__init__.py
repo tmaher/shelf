78e69a1384341117e36f4c285e674028706b2be6
tom maher <tmaher@pw0n.ie>
2024-04-21 15:15:03 +0100

    wip

--- src/shelf/__init__.py
+++ src/shelf/__init__.py
@@ -1,8 +1,8 @@
 # src/shelf/__init__.py
 __version__ = "0.1.0"
 
-from .shelf_feedgen import ShelfError
+from .shelf_feed_generator import ShelfError, ShelfFeedGenerator
 
 print("I'm in danger!!!")
 
-__all__ = ["__version__", "ShelfFeedgen"]
+__all__ = ["__version__", ShelfFeedGenerator, ShelfError]
---
+++ src/shelf/shelf_feed_generator.py
@@ -0,0 +1,19 @@
+# src/shelf/shelf_feed_generator.py
+
+# from feedgen.ext.base import BaseExtension
+from feedgen.feed import FeedGenerator
+
+
+class ShelfError(RuntimeError):
+    """
+This is a class
+    """
+
+
+class ShelfFeedGenerator(FeedGenerator):
+    """
+This is another class
+    """
+    def __init__(
+    ):
+        True
