e949d485f4ca03a66d04c64cd6f386936ec64b2c
tom maher <tmaher@pw0n.ie>
2024-04-21 17:19:41 +0100

    wip

--- tests/test_shelf.py
+++ tests/test_shelf.py
@@ -7,13 +7,15 @@ class TestSFG:
     def my_sfg(self):
         sfg = ShelfFeedGenerator()
         sfg.title('bob the angry podcast')
+        sfg.id('https://bob.the.angry.podcast/')
         return sfg
 
     def test_create(self, my_sfg):
         assert isinstance(my_sfg, ShelfFeedGenerator)
 
-    def test_title(self, my_sfg):
+    def test_basic_attrs(self, my_sfg):
         assert my_sfg.title() == 'bob the angry podcast'
+        assert my_sfg.id() == 'https://bob.the.angry.podcast/'
 
     def test_ext(self, my_sfg):
         True
